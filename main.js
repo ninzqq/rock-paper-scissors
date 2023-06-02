5dbfb0c329348dd2b14c98b003c0cf8f10add9fa
Janne Niinisalo <janne.niinisalo@gmail.com>
2023-06-03 02:19:23 +0300

    game done

--- main.js
+++ main.js
@@ -18,33 +18,58 @@ function playerSelection() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let score = 0;
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
             console.log("It's a TIE");
         } else if (computerSelection === "scissors") {
             console.log("You WIN. Rock beats Scissors");
+            score++;
         } else if ( computerSelection === "paper") {
             console.log("You LOSE. Paper beats Rock");
+            score--;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "paper") {
             console.log("It's a TIE");
         } else if (computerSelection === "rock") {
             console.log("You WIN. Paper beats Rock");
+            score++;
         } else if ( computerSelection === "scissors") {
             console.log("You LOSE. Scissors beats Paper");
+            score--;
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "scissors") {
             console.log("It's a TIE");
         } else if (computerSelection === "paper") {
             console.log("You WIN. Scissors beats Paper");
+            score++;
         } else if ( computerSelection === "rock") {
             console.log("You LOSE. Rock beats Scissors");
+            score--;
         }
     }
-
+    return score;
 }
 
+function game() {
+    let player = 0;
+    let computer = 0;
+    let tie = 0;
+    for (i = 0; i < 5; i++) {
+        let score = playRound(playerSelection(), getComputerChoice());
+        if (score === 1) {
+            player++;
+        } else if (score === -1) {
+            computer++;
+        } else {
+            tie++;
+        }
+    }
+    console.log("Player won " + player + " times");
+    console.log("Computer won " + computer + " times");
+    console.log("It was a TIE " + tie + " times");
+}
 
-
+game();
